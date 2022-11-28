6432d80fdfca60065180c5e45b73e919069c79c1
akash.a.singh <113594078+arduino-akash@users.noreply.github.com>
2022-11-28 14:23:37 +0530

    Initial Commit

---
+++ PinchFramework/PinchFramework.h
@@ -0,0 +1,18 @@
+//
+//  PinchFramework.h
+//  PinchFramework
+//
+//  Created by Singh, Akash | RIEPL on 28/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PinchFramework.
+FOUNDATION_EXPORT double PinchFrameworkVersionNumber;
+
+//! Project version string for PinchFramework.
+FOUNDATION_EXPORT const unsigned char PinchFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PinchFramework/PublicHeader.h>
+
+
